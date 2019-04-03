cd07bb517c2c9c959a6b88c9f24ce973b331825e
Mark Stock <markjstock@gmail.com>
2019-04-03 12:05:53 -0400

    Ease MSVC builds with better command line, VS integration

--- src/Kernels.h
+++ src/Kernels.h
@@ -7,7 +7,6 @@
 
 #pragma once
 
-#define _USE_MATH_DEFINES
 #include <cmath>
 
 
--- src/Points.h
+++ src/Points.h
@@ -27,7 +27,6 @@
 #include <optional>
 #include <random>
 #include <cassert>
-#define _USE_MATH_DEFINES
 #include <cmath>
 #include <algorithm>
 
